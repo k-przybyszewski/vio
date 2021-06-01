b44ed23201ec97a0fea8d9514b77874b67958d8a
Krzysztof Przybyszewski <k.m.przybyszewski@gmail.com>
2021-06-01 10:09:50 +0200

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-const cfg = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
-// Expo CLI will await this method so you can optionally return a promise.
-module.exports = async function(env, argv) {
-  const config = await cfg(env, argv);
-  // If you want to add a new alias to the config.
-  config.resolve.alias['moduleA'] = 'moduleB';
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Customize the config before returning it.
 
   // Maybe you want to turn off compression in dev mode.
-  if (config.mode === 'development') {
+  if (config.mode === "development") {
     config.devServer.compress = false;
   }
 
   // Or prevent minimizing the bundle when you build.
-  if (config.mode === 'production') {
+  if (config.mode === "production") {
     config.optimization.minimize = false;
   }
 
